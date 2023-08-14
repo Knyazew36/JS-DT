29b6dd3fc2d39db76c16bbba803ccaa120c6009d
DESKTOP-FR5AHN7\knyaz <knyazew.sv@gmail.com>
2023-08-14 22:35:03 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { FetchUsers } from "./api/FetchUsers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+
+  async componentDidMount() {
+    const users = await FetchUsers();
+    this.setState({ users: users });
+  }
+
+  render() {
+    const { users } = this.state;
+    //Каждый второй
+    const secondElements = users.filter((user, index) => {
+      return index % 2 === 1;
+    });
+    // Присваиваем null
+    const modifiedData = secondElements.map((user, index) => {
+      if ((index + 1) % 3 === 0) {
+        user.address.geo.lat = null;
+        user.address.geo.lng = null;
+      }
+      return user;
+    });
+
+    // id
+    const userIds = modifiedData.reduce((acc, user) => {
+      return acc + ` ${user.id}`;
+    }, "");
+
+    return (
+      <div>
+        <h1>Список юзеров</h1>
+        <div>ID выведенных юзеров:{userIds} </div>
+
+        <ul>
+          {modifiedData.map(({ id, name, username, email, address }) => (
+            <li key={id}>
+              {`${name}, ${username} ${email}`}{" "}
+              <a
+                href={email}
+                onClick={(event) => {
+                  event.preventDefault();
+                  console.log(email);
+                }}
+              >
+                ссылка email
+              </a>{" "}
+              <br />
+              lat:{" "}
+              {address.geo && address.geo.lat != null
+                ? address.geo.lat
+                : "не определено"}
+              , lng:{" "}
+              {address.geo && address.geo.lng != null
+                ? address.geo.lng
+                : "не определено"}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
